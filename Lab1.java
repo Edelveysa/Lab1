40c45280b4c30643e24abfa82fd9bab0d4bd94a0
Edelveysa <51116291+Edelveysa@users.noreply.github.com>
2020-09-29 11:57:04 +0300

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -1,45 +1,71 @@
-class Lab1{
-    
-    public static void main(String[] args){
+import java.lang.Math;
 
-        short firstnumber = 7;
-        short[] d1 = new short[8];
+class Lab1 {
+    public static void main(String[] agrs) {
+        Matem maths = new Matem();
+        short[] arrayShort = maths.arrayShort();
+        System.out.println();
+        float[] arrayFloat = maths.arrayFloat();
+        System.out.println();
+        double[][] arrayDouble = maths.arrayDouble(arrayShort, arrayFloat);
+        maths.PrintArrayDouble(arrayDouble);
+    }
+
+}
+
+class Matem{
+    short startFromShort = 7;
+    final float minFloat = -6.0F;
+    final float maxFloat = 5.0F;
+
+    short[] arrayShort(){
+        short[] d = new short[8];
         for(int i = 0; i < 8; i++){
-            d1[i] = firstnumber;
-            firstnumber += 2;
+            d[i] = startFromShort;
+            startFromShort += 2;
         }
-
+        System.out.println();
+        return d;
+    }
+    
+    float[] arrayFloat(){
         float[] x = new float[14];
-        for(int j = 0; j<14; j++){x[j] = (float)(Math.random()*11-6);}
-
+        for(int j = 0; j<x.length; j++){x[j] = Randomize();}
+        System.out.println();
+        return x;
+    }
+    
+    double[][] arrayDouble(short[] arrayShort, float[] arrayFloat) {
         double[][] d = new double[8][14];
-        for(int i = 0; i<8; i++){
-            for(int j = 0; j<14; j++){
-                switch (d1[i]){
+        for(int i = 0; i < d.length; i++){
+            for(int j = 0; j < d[i].length; j++){
+                switch (arrayShort[i]){
                     case 7:
-                        d[i][j]=Math.atan(Math.sin(Math.tan(Math.log(Math.abs(x[j])))));
+                        d[i][j]=ScaryMath1(arrayFloat[j]);
                         break;
                     case 15:
                     case 17:
                     case 19:
                     case 21:
-                        d[i][j]=Math.cos(Math.tan(Math.pow(x[j], x[j]/2)));
+                        d[i][j]=ScaryMath2(arrayFloat[j]);
                         break;
-                    default: d[i][j]=Math.pow( (0.75+Math.pow(0.75/((Math.sin(x[j]))/(Math.log(Math.abs(x[j]))-2)+1),Math.cos(Math.log(Math.abs(x[j]))))) / (Math.asin(Math.pow(Math.cos(Math.cos(x[j]+1)), 2))), 3);
+                    default: d[i][j]=ScaryMath3(arrayFloat[j]);
                         break;
                 }
             }
         }
-
-        for(int i = 0; i<8; i++){
-            for(int j = 0; j<14; j++){
-                System.out.print(String.format("%.5f", d[i][j])+"\t");
-
+        return d;
+    }
+    void PrintArrayDouble(double[][] d) {
+        for (int i = 0; i < d.length; i++) {
+            for (int j = 0; j < d[i].length; j++) {
+                System.out.print(String.format("%.5f", d[i][j]) + "\t");
             }
             System.out.println();
         }
-
-
-
     }
+    float Randomize(){return (float)(Math.random()*(minFloat - maxFloat)- minFloat);}
+    double ScaryMath1(float number){return Math.atan(Math.sin(Math.tan(Math.log(Math.abs(number)))));}
+    double ScaryMath2(float number) { return Math.cos(Math.tan(Math.pow(number, number/2)));}
+    double ScaryMath3(float number){ return Math.pow((0.75+Math.pow(0.75/((Math.sin(number))/(Math.log(Math.abs(number))-2)+1),Math.cos(Math.log(Math.abs(number))))) / (Math.asin(Math.pow(Math.cos(Math.cos(number+1)), 2))), 3);}
 }
