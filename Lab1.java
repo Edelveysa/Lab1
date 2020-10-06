980dbea3bf11cbba4dce752686f4a7b975b93410
Edelveysa <51116291+Edelveysa@users.noreply.github.com>
2020-10-06 17:10:30 +0300

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -2,11 +2,8 @@ class Lab1 {
     public static void main(String[] agrs) {
         Matem maths = new Matem();
         short[] arrayShort = maths.arrayShort();
-        System.out.println();
         float[] arrayFloat = maths.arrayFloat();
-        System.out.println();
         double[][] arrayDouble = maths.arrayDouble(arrayShort, arrayFloat);
         maths.PrintArrayDouble(arrayDouble);
     }
-
 }
